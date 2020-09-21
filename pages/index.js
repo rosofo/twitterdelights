714c69ad4e036941bf6b017a7f62ef0c1bdc11e8
Walid <hello@walidvb.com>
2020-09-21 03:37:21 +0200

    adds a page title

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ const MediaList = ({ tweets }) => {
 }
 
 const SearchInput = ({ username, setUsername }) => {
-  return <input className="border-b border-gray-400 text-center py-1 px-2" placeholder="Enter a twitter handle" type={'text'} value={username} onChange={({ target: { value } }) => setUsername(value)}/>
+  return <input autofocus="true"  className="focus:outline-none focus:border-gray-700 border-b border-gray-400 text-center py-1 px-2" placeholder="Type a twitter handle" type={'text'} value={username} onChange={({ target: { value } }) => setUsername(value)}/>
 }
 
 const Results = ({ username, tweets: { meta, data } }) => {
@@ -34,8 +34,15 @@ const Results = ({ username, tweets: { meta, data } }) => {
 }
 
 const NoQuery = ({ setUsername }) => {
-  return <div>
-    Examples: <span className="underline hover:no-underline cursor-pointer" onClick={() => setUsername('sk33mask')}>sk33mask</span> or <span className="underline hover:no-underline cursor-pointer" onClick={() => setUsername('delightsdiggers')}>delightsdiggers</span>
+  return <div className="text-center">
+    <div className="font-bold mb-2 text-2xl">
+      Twitter Delights
+    </div>
+    See all youtube videos posted by twitter users
+    <br />
+    <span className="text-gray-700">
+      Examples: <span className="underline hover:no-underline cursor-pointer" onClick={() => setUsername('sk33mask')}>sk33mask</span> or <span className="underline hover:no-underline cursor-pointer" onClick={() => setUsername('delightsdiggers')}>delightsdiggers</span>
+    </span>
   </div>
 }
 const initialTweetsState = {
@@ -56,8 +63,11 @@ export default function Home() {
     }
     (async () => {
       setLoading(true)
-      const { data } = await Axios.get(`/api/getTweets?username=${debouncedUsername}`)
-      setTweets(data)
+      try{
+        const { data } = await Axios.get(`/api/getTweets?username=${debouncedUsername}`)
+        setTweets(data)
+      } catch(err){
+      }
       setLoading(false)
     })()
   }, [debouncedUsername])
@@ -71,11 +81,11 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <div className="my-16 mt-32">
+        <div className={`my-16 mt-32 ${styles.appear}`}>
           <SearchInput username={username} setUsername={setUsername} />
         </div>
         {
-          loading ? <div><BoxLoading color="#ABC" /></div>
+          loading ? <div className="relative"><BoxLoading color="#ABC" /></div>
           : username.length ? 
             <Results username={username} tweets={tweets} /> 
             : <NoQuery setUsername={setUsername} /> 
@@ -83,11 +93,8 @@ export default function Home() {
       </main>
 
       <footer className={styles.footer}>
-          <div className="underline">
-            Twitter Delights
-          </div>
-          <div>
-            Made with ♥️ by&nbsp; <a href="http://walidvb.com" target="_blank">walidvb</a>
+          <div className="text-gray-600 text-sm">
+            Made with ♥️ by&nbsp;<a className="underline hover:no-underline" href="http://walidvb.com" target="_blank">walidvb</a>, author of <a className="underline hover:no-underline" href="https://www.diggersdelights.net" target="_blank">diggersdelights</a>
           </div>
       </footer>
     </div>

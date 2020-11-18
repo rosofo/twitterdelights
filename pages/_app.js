e475083de581f69381f5a2e8e754f93782d14fef
Walid <hello@walidvb.com>
2020-11-18 12:27:50 -0300

    added fullstory

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,10 @@
 import '../styles/globals.css'
 import useGA from  '../src/hooks/useGA'
+import useFullStory from '../src/hooks/useFullStory';
 
 function MyApp({ Component, pageProps }) {
   useGA()
+  useFullStory()
   return <>
     <Component {...pageProps} />
   </>
